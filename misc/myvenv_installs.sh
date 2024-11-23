7157eddf5dfc7a4bbd13d6a7ac6165c0bffe9651
jyhong <jyhong@caltech.edu>
2024-11-23 02:32:07 -0800

    added some misc stuff

---
+++ misc/myvenv_installs.sh
@@ -0,0 +1,5 @@
+# dependencies for CFL
+pip install cfl
+pip install optuna
+pip install tensorflow==2.15.0
+pip install keras==2.15.0
---
+++ misc/myvenv_setup.sh
@@ -0,0 +1,2 @@
+python3.11 -m venv myvenv
+source myvenv/bin/activate
