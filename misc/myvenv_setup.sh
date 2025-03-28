0b661d81dbfeb4abb0d76ce9141d8709195da425
jyhong <jyhong@caltech.edu>
2025-03-27 20:51:25 -0700

    nothing happened

--- misc/myvenv_setup.sh
+++ misc/myvenv_setup.sh
@@ -1,4 +1,6 @@
 python3.11 -m venv myvenv
-source myvenv/bin/activate
-# for windows: 
+source myvenv/bin/activate 
+# for windows:
 # myvenv\Scripts\activate
+
+# deactivate in terminal to deactivate
